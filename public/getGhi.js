f60e5bf717b8f40605666f0f73e7c150256a1142
Sibiya <sibiyasn02@gmail.com>
2022-09-26 13:17:10 +0200

    initial commit

--- public/getGhi.js
+++
@@ -1,7 +0,0 @@
-function getGhi() {
-    if (navigator.geolocation) {
-      navigator.geolocation.watchPosition(showPosition);
-    } else {
-      div.innerHTML = "The Browser Does not Support Geolocation";
-    }
-  }
